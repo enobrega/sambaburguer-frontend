9fd4e35328c594a843ca71e49a5116e6fe15c519
Eduardo Nóbrega <enobrega@gmail.com>
2025-05-11 09:10:29 -0300

    updates

--- src/app/cardapio/page.tsx
+++ src/app/cardapio/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 import { FaWhatsapp } from "react-icons/fa";
 import Link from "next/link";
@@ -6,10 +7,12 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <img
+        <Image
           className={styles.logo}
           src="/image/menu.png"
           alt="Cardápio"
+          width={900}
+          height={1360}
         />
 
         <a href="https://wa.me/5581998928225" className={styles.cta}>
